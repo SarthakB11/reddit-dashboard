ac0007dc04eb521ff71da511fae8b38486fd36cc
SarthakB11 <sarthak.bhardwaj21b@iiitg.ac.in>
2025-03-03 08:07:14 +0530

    frontend deployment stuff

--- social-media-dashboard/next.config.ts
+++ social-media-dashboard/next.config.ts
@@ -1,7 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false, // Disable strict mode to reduce hydration warnings
-  swcMinify: true,
 };
 
 export default nextConfig;
